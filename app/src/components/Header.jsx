5561b812647446f2f631bf22a4096fbfc27002ec
abdulqaderharitani <abdulqader@onlinepaymentplatform.com>
2023-12-03 09:52:54 +0100

    defualt header and footer

--- app/src/components/Header.jsx
+++ app/src/components/Header.jsx
@@ -38,9 +38,7 @@ class Header extends Component {
                             </li>
                         </ul>
                         <form className="d-flex" role="search">
-                            <input className="form-control me-2" type="search" placeholder="Search"
-                                   aria-label="Search" />
-                            <button className="btn btn-outline-success" type="submit">Search</button>
+                            <button className="btn btn-outline-success" type="submit">Login</button>
                         </form>
                     </div>
                 </div>
